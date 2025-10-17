58d9b918ac520253387763378ee7a9a1a1c93c5d
tilak-c <66167345+tilak-c@users.noreply.github.com>
2025-10-17 14:35:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let tableBody=document.querySelector("#table tbody");
+const submitElement=document.getElementById('submit');
+submitElement.addEventListener("click",function(){
+	const firstname=document.getElementById('firstname').value;
+const lastname=document.getElementById('lastname').value;
+const phonenumber=document.getElementById('phonenumber').value;
+const emailid=document.getElementById('emailid').value;
+
+		if(!firstname || !lastname||!phonenumber||!emailid){
+			alert("Please fill all fields");
+			return;
+		}
+	
+	let newRow=document.createElement('tr');
+	newRow.innerHTML=
+		`<td>${firstname}</td>
+		<td>${lastname}</td>
+		<td>${phonenumber}</td>
+		<td>${emailid}</td>
+		`
+	tableBody.appendChild(newRow);
+			document.getElementById('firstname').value = '';
+			document.getElementById('lastname').value = '';
+			document.getElementById('phonenumber').value = '';
+			document.getElementById('emailid').value = '';
+})
